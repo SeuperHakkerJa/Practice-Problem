7990448d30ae4cae25de9b7d5b536d0150af00e5
Xiaochen Li <jacobxiaochen@sina.com>
2019-03-20 23:11:12 -0700

    update unique subset in java

---
+++ UniqSubSet.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class  UniqSubSet{
+ 
+  public List<List<Integer>> findSubSet(int[] num) {
+   List<List<Integer>> unique_subset = new ArrayList<>();
+   List<Integer> list = new ArrayList<>();
+
+   return unique_subset;
+  
+  
+  }
+
+
+
+
+  private void subsetHelper(List<List<Integer>> result, List<Integer> list, 
+                            int[] num, int pos){
+    result.add(new ArrayList<Integer>(list));
+    System.out.println(result);
+
+
+    for (int i = pos; i < num.length; ++i) {
+      if (i != pos && num[i] == num[i-1]) {
+        continue;
+      }
+      list.add(num[i]);
+      subsetHelper(result,list,num,i+1);
+      list.remove(list.size()-1);
+    }
+  }
+
+}
